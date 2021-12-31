f02532646e1266f20068fd9984c68f484eca5512
Rhaidzsal Ali <rhaidzsalali@gmail.com>
2021-12-31 11:19:07 +0800

    Add validate network.

--- src/hooks/useWallet.tsx
+++ src/hooks/useWallet.tsx
@@ -6,6 +6,58 @@ const useWallet = () => {
   const [currentAccount, setCurrentAccount] = useAtom(accountAtom)
   const toast = useToast()
 
+  const validateNetwork = async (): Promise<boolean> => {
+    const { ethereum } = window as any
+
+    if (ethereum) {
+      const isDevelopment = process.env.NODE_ENV === 'development'
+      const chainId = await ethereum.request({ method: 'eth_chainId' })
+
+      const rinkebyChainId = '0x4'
+      const metisStardustChainId = '0x24C'
+
+      if (isDevelopment && chainId !== rinkebyChainId) {
+        toast({
+          title: (
+            <Text fontSize="sm" className="plex">
+              Wrong network.
+            </Text>
+          ),
+          description: (
+            <Text fontSize="xs" className="plex">
+              Please use the Rinkeby Network.
+            </Text>
+          ),
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        })
+        return false
+      }
+
+      if (!isDevelopment && chainId !== metisStardustChainId) {
+        toast({
+          title: (
+            <Text fontSize="sm" className="plex">
+              Wrong network.
+            </Text>
+          ),
+          description: (
+            <Text fontSize="xs" className="plex">
+              Please use the Metis Stardust Network.
+            </Text>
+          ),
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        })
+        return false
+      }
+      return true
+    }
+    return false
+  }
+
   const connectWallet = async () => {
     try {
       const { ethereum } = window as any
@@ -29,6 +81,10 @@ const useWallet = () => {
         return
       }
 
+      const isValidNetwork = await validateNetwork()
+
+      if (!isValidNetwork) return
+
       const accounts = await ethereum.request({
         method: 'eth_requestAccounts',
       })
@@ -47,11 +103,22 @@ const useWallet = () => {
         setCurrentAccount(accounts[0])
       }
     } catch (error) {
+      toast({
+        title: (
+          <Text fontSize="sm" className="plex">
+            Connection failed.
+          </Text>
+        ),
+
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
       console.error(error)
     }
   }
 
-  return { currentAccount, connectWallet }
+  return { currentAccount, connectWallet, validateNetwork }
 }
 
 export default useWallet
